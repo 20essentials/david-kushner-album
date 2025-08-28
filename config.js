145c2695362b52dc53935cc63ffa98ff6fb342db
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 22:55:33 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'DAVID KUSHNER';
+export let nameSongs = [
+	"DAYLIGHT (SLOWED + REVERB)", /*1*/
+	"DEAD MAN", /*2*/
+	"DAYLIGHT", /*3*/
+	"MR. FORGETTABLE", /*4*/
+	"MISERABLE MAN ", /*5*/
+	"BURN", /*6*/
+	"ELK GROVE", /*7*/
+	"HERO", /*8*/
+	"SKIN AND BONES", /*9*/
+	"HUMANKIND", /*10*/
+	"OOOWOOO", /*11*/
+	"LOOK BACK & LAUGH", /*12*/
+	"CANNON BEACH", /*13*/
+	"CIGARETTES", /*14*/
+	"HERO (CINEMATIC)", /*15*/
+	"DAYLIGHT (ACOUSTIC)", /*16*/
+	"HERO (SLOWED + REVERB)", /*17*/
+	"SKIN AND BONES (RADIO EDIT)", /*18*/
+	"DAYLIGHT (CINEMATIC)", /*19*/
+	"DAYLIGHT (INSTRUMENTAL)", /*20*/
+]
